d7509f793f2506af8a536a4536a3027521d41359
thearjun <33110520+theArjun@users.noreply.github.com>
2024-02-27 00:47:06 +0545

    Update keymaps

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -1,4 +1,44 @@
 return {
   "numToStr/Comment.nvim",
+  config = function()
+    require('Comment').setup({
+      -- Sensible options for Comment.nvim
+      padding = true, -- Add a space between the comment symbol and the line content
+      sticky = true, -- Whether the cursor should stay at the current line or not
+      ignore = nil, -- Lines matching the pattern will not be commented
+      toggler = {
+        line = 'gcc', -- Toggle comment for the current line
+        block = 'gbc', -- Toggle comment for the current block
+      },
+      opleader = {
+        line = 'gc', -- Toggle comment for lines in operator pending mode
+        block = 'gb', -- Toggle comment for blocks in operator pending mode
+      },
+      extra = {
+        above = 'gcO', -- Add comment on the line above
+        below = 'gco', -- Add comment on the line below
+        eol = 'gcA', -- Add comment at the end of line
+      },
+      mappings = {
+        -- Basic mapping, set to false to disable
+        basic = true, -- Enable all basic mappings
+        extra = true, -- Enable extra mappings
+        extended = false, -- Disable extended mappings
+      },
+      -- Uncomment below if you want to set specific language options
+      -- pre_hook = function(ctx)
+      --   -- Example: return '//' for tsx files instead of the default
+      --   if vim.bo.filetype == 'typescriptreact' then
+      --     return '//'
+      --   end
+      -- end,
+    })
+
+    -- Optional: Set keymaps manually if you want to customize further
+    --[[ local map = vim.api.nvim_set_keymap
+    local opts = {noremap = true, silent = true}
+    map('n', 'gcc', '<cmd>CommentToggle<CR>', opts)
+    map('v', 'gc', '<cmd>CommentToggle<CR>', opts) ]]
+  end,
   opts = {}
 }
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,5 +1,35 @@
 return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
-  config = true,
+  config = function()
+    require('gitsigns').setup {
+      signs = {
+        add = {text = '+'},
+        change = {text = '~'},
+        delete = {text = '_'},
+        topdelete = {text = '‾'},
+        changedelete = {text = '~'},
+      },
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+        
+        -- Navigation
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', ']c', '<cmd>lua require"gitsigns".next_hunk()<CR>', {silent = true, noremap = true})
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '[c', '<cmd>lua require"gitsigns".prev_hunk()<CR>', {silent = true, noremap = true})
+
+        -- Actions
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>hs', '<cmd>lua require"gitsigns".stage_hunk()<CR>', {silent = true, noremap = true})
+        vim.api.nvim_buf_set_keymap(bufnr, 'v', '<leader>hs', '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>', {silent = true, noremap = true})
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>hr', '<cmd>lua require"gitsigns".reset_hunk()<CR>', {silent = true, noremap = true})
+        vim.api.nvim_buf_set_keymap(bufnr, 'v', '<leader>hr', '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>', {silent = true, noremap = true})
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>hS', '<cmd>lua require"gitsigns".stage_buffer()<CR>', {silent = true, noremap = true})
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>hu', '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>', {silent = true, noremap = true})
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>hR', '<cmd>lua require"gitsigns".reset_buffer()<CR>', {silent = true, noremap = true})
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>hp', '<cmd>lua require"gitsigns".preview_hunk()<CR>', {silent = true, noremap = true})
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>hb', '<cmd>lua require"gitsigns".blame_line(true)<CR>', {silent = true, noremap = true})
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>hd', '<cmd>lua require"gitsigns".diffthis()<CR>', {silent = true, noremap = true})
+        -- More mappings can be added here
+      end,
+    }
+  end,
 }
