d5d1931515942fa403267bd798625d52a41582f1
thearjun <33110520+theArjun@users.noreply.github.com>
2024-02-27 00:23:15 +0545

    Syncing config on 2024-02-27-00-23-15 on the-macbook-pro.local

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,9 +7,12 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   keys = {
-    {
-      '<C-n>', "<cmd>Neotree toggle position=left <CR>", desc="toggle neotree"
-    },
+    { '<C-n>', "<cmd>Neotree toggle position=left <CR>", desc="Toggle Neo-tree" },
+    -- Additional keymaps
+    { '<leader>nf', "<cmd>Neotree toggle position=float <CR>", desc="Toggle Neo-tree in floating window" },
+    { '<leader>nd', "<cmd>Neotree toggle position=left reveal_force_cwd=true <CR>", desc="Toggle Neo-tree and reveal current file" },
+    { '<leader>nb', "<cmd>Neotree toggle position=left source=buffers <CR>", desc="Toggle Neo-tree with buffer list" },
+    -- You can add more mappings as needed
   },
   config = function()
     local neotree = require("neo-tree")
--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -1,8 +1,18 @@
 return {
   "folke/zen-mode.nvim",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
+  config = function()
+    -- Sensible keymap for toggling Zen Mode
+    local map = vim.api.nvim_set_keymap
+    local opts = { noremap = true, silent = true }
+
+    -- Toggle Zen Mode with a keymap, for example: <leader>z
+    map('n', '<leader>z', '<cmd>ZenMode<CR>', opts)
+
+    -- Optional: Configure Zen Mode further if needed
+    require("zen-mode").setup({
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the Zen Mode documentation for configuration options
+    })
+  end
 }
