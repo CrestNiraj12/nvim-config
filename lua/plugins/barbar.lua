cd7f7d6a64426d45e43a21293f9c4fedb9484241
thearjun <33110520+theArjun@users.noreply.github.com>
2024-02-27 00:12:05 +0545

    Shift tabs

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,14 +1,24 @@
+
 return {
   'romgrk/barbar.nvim',
   dependencies = {
     'lewis6991/gitsigns.nvim',
     'nvim-tree/nvim-web-devicons',
   },
-  init = function() vim.g.barbar_auto_setup = false end,
+  init = function()
+    vim.g.barbar_auto_setup = false
+    -- Define key mappings here. Example:
+    local map = vim.api.nvim_set_keymap
+    local opts = { noremap = true, silent = true }
+    map('n', '<Tab>', ':BufferNext<CR>', opts)
+    map('n', '<S-Tab>', ':BufferPrevious<CR>', opts)
+    map('n', '<leader>bc', ':BufferClose<CR>', opts)
+    -- Add more mappings as needed
+  end,
   opts = {
     sidebar_filetypes = {
       ['neo-tree'] = { event = 'BufWipeout' }
     }
   },
-  version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  version = '^1.0.0',
 }
