e11ab395e8a3bddffa0c251cf24fbe9f9a369563
thearjun <33110520+theArjun@users.noreply.github.com>
2024-02-28 20:09:26 +0545

    Seitch between splits

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,3 +6,9 @@ set('n', '<leader>hs', ':split<CR>', { noremap = true, silent = true, desc = 'Ho
 
 -- Vertical split
 set('n', '<leader>vs', ':vsplit<CR>', { noremap = true, silent = true, desc = 'Vertical Split' })
+
+-- Switch between splits with Ctrl + h/j/k/l
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {noremap = true, silent = true})
