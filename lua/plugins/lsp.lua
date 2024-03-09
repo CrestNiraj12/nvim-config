170e454618cc3decf91a44839a23728f027d3528
thearjun <33110520+theArjun@users.noreply.github.com>
2024-03-09 09:15:48 +0545

    fix: remove trailing comma

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -17,7 +17,6 @@ return {
 					local null_ls = require("null-ls")
 					null_ls.setup({
 						sources = {
-							null_ls.builtins.formatting.prettier,
 							null_ls.builtins.code_actions.gitsigns,
 						},
 					})
