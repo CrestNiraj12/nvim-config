53a41163d4920e8df7df3c0878afece4e24090e8
thearjun <33110520+theArjun@users.noreply.github.com>
2024-03-18 12:45:15 +0545

    chore: format codebase

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,3 @@
-
 local set = vim.keymap.set
 
 -- Horizontal split
@@ -8,10 +7,10 @@ set('n', '<leader>hs', ':split<CR>', { noremap = true, silent = true, desc = 'Ho
 set('n', '<leader>vs', ':vsplit<CR>', { noremap = true, silent = true, desc = 'Vertical Split' })
 
 -- Switch between splits with Ctrl + h/j/k/l
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
 
 -- Buffer Close
 set('n', '<leader>bd', ':bd<CR>', { noremap = true, silent = true, desc = 'Buffer Close' })
