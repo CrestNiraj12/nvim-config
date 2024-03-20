8a2844915d45ecf23a40041b4f2e2f8e2d7248e3
thearjun <33110520+theArjun@users.noreply.github.com>
2024-03-20 12:45:58 +0545

    chore: add buffer close keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -10,7 +10,9 @@ vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true 
 vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
 
--- Buffer Close
-set('n', '<leader>bd', ':bd<CR>', { noremap = true, silent = true, desc = 'Buffer Close' })
+-- Buffer Close All
+set('n', '<leader>ba', ':bufdo bd<CR>', { noremap = true, silent = true, desc = 'Buffer Close All' })
 -- Save file
 set('n', '<C-s>', ':w<CR>', { noremap = true, silent = true, desc = 'Save file' })
+-- Close file
+set('n', '<leader>q', ':q<CR>', { noremap = true, silent = true, desc = 'Close file' })
