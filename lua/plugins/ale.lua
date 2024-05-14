8e592eb0f9892c6ce7c0725d9270fe7e3c4403f9
Niraj Shrestha <crestniraj@gmail.com>
2024-05-14 11:59:05 +0545

    :sparkles: feat: added linter and fixer for typescript react

--- lua/plugins/ale.lua
+++ lua/plugins/ale.lua
@@ -1,27 +1,31 @@
 return {
-	"dense-analysis/ale",
-	event = "BufRead", -- Adjust the event for when you want ALE to load
-	config = function()
-		-- ALE basic configuration
-		vim.g.ale_lint_on_enter = 1             -- Lint files when they are opened
-		vim.g.ale_lint_on_save = 1              -- Lint files when they are saved
-		vim.g.ale_lint_on_text_changed = 'always' -- Lint files as you type
+  "dense-analysis/ale",
+  event = "BufRead", -- Adjust the event for when you want ALE to load
+  config = function()
+    -- ALE basic configuration
+    vim.g.ale_lint_on_enter = 1               -- Lint files when they are opened
+    vim.g.ale_lint_on_save = 1                -- Lint files when they are saved
+    vim.g.ale_lint_on_text_changed = 'always' -- Lint files as you type
 
-		-- Additional ALE settings...
-		-- Set up any ALE linters, fixers, or other options as needed
-		-- Example: Enable specific linters
-		vim.g.ale_linters = {
-			python = { 'flake8', 'mypy', 'refurb', 'bandit'},
-			dart = { 'dartanalyzer' }
-			-- Add other languages and linters as needed
-		}
+    -- Additional ALE settings...
+    -- Set up any ALE linters, fixers, or other options as needed
+    -- Example: Enable specific linters
+    vim.g.ale_linters = {
+      python = { 'flake8', 'mypy', 'refurb', 'bandit' },
+      dart = { 'dartanalyzer' },
+      typescript = { 'tsserver', 'eslint' },
+      typescriptreact = { 'tsserver', 'eslint' },
+      -- Add other languages and linters as needed
+    }
 
-		-- Example: Enable ALE fixers
-		vim.g.ale_fixers = {
-			python = { 'ruff_format', 'isort' },
-			dart = { 'dartfmt' }
-			-- Add other languages and fixers as needed
-		}
-		vim.g.ale_fix_on_save = 1 -- Automatically fix errors on save
-	end,
+    -- Example: Enable ALE fixers
+    vim.g.ale_fixers = {
+      python = { 'ruff_format', 'isort' },
+      dart = { 'dartfmt' },
+      typescript = { 'eslint' },
+      typescriptreact = { 'eslint' },
+      -- Add other languages and fixers as needed
+    }
+    vim.g.ale_fix_on_save = 1 -- Automatically fix errors on save
+  end,
 }
