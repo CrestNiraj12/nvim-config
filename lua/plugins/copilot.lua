872506bbb576df6246f028eff97502aa9376e5e9
Niraj Shrestha <crestniraj@gmail.com>
2025-05-21 12:54:17 +0545

    updated copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,21 @@
 return {
-  "github/copilot.vim",
+  "zbirenbaum/copilot.lua",
+  event = "InsertEnter",
+  opts = {
+    suggestion = {
+      auto_trigger = true,
+      debounce = 100, -- reduce rapid firing edits
+      keymap = {
+        accept = "<C-l>",
+        dismiss = "<C-]>",
+      },
+    },
+    filetypes = {
+      dart = true, -- make sure Copilot works in Dart
+      markdown = false,
+    },
+  },
+  config = function(_, opts)
+    require("copilot").setup(opts)
+  end,
 }
