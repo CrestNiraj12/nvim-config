31e6dc720595b1d98f1a9e6ea94e49a39644cb5c
Niraj Shrestha <crestniraj@gmail.com>
2025-05-21 14:15:32 +0545

    updates

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,6 +1,9 @@
 return {
   'saghen/blink.cmp',
   version = '*',
+  dependencies = {
+    "giuxtaposition/blink-cmp-copilot",
+  },
   opts = {
     keymap = {
       preset = 'default',
@@ -10,7 +13,7 @@ return {
       ["<CR>"] = { 'accept', 'fallback' },
     },
     sources = {
-      default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
       providers = {
         snippets = {
           opts = {
@@ -25,6 +28,21 @@ return {
           module = "lazydev.integrations.blink",
           -- make lazydev completions top priority (see `:h blink.cmp`)
           score_offset = 100,
+        },
+        copilot = {
+          name = "copilot",
+          module = "blink-cmp-copilot",
+          score_offset = 100,
+          async = true,
+          transform_items = function(_, items)
+            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+            local kind_idx = #CompletionItemKind + 1
+            CompletionItemKind[kind_idx] = "Copilot"
+            for _, item in ipairs(items) do
+              item.kind = kind_idx
+            end
+            return items
+          end,
         },
       },
     },
@@ -49,6 +67,39 @@ return {
     appearance = {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = 'mono'
+      kind_icons = {
+        Copilot = "",
+        Text = '󰉿',
+        Method = '󰊕',
+        Function = '󰊕',
+        Constructor = '󰒓',
+
+        Field = '󰜢',
+        Variable = '󰆦',
+        Property = '󰖷',
+
+        Class = '󱡠',
+        Interface = '󱡠',
+        Struct = '󱡠',
+        Module = '󰅩',
+
+        Unit = '󰪚',
+        Value = '󰦨',
+        Enum = '󰦨',
+        EnumMember = '󰦨',
+
+        Keyword = '󰻾',
+        Constant = '󰏿',
+
+        Snippet = '󱄽',
+        Color = '󰏘',
+        File = '󰈔',
+        Reference = '󰬲',
+        Folder = '󰉋',
+        Event = '󱐋',
+        Operator = '󰪚',
+        TypeParameter = '󰬛',
+      },
     },
   },
 }
