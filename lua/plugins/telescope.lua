572efa55bfe77daa2113a0c756adcfc099ac306e
Niraj Shrestha <crestniraj@gmail.com>
2025-05-26 14:01:34 +0545

    chore: ignore freezed.dart and g.dart

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -34,10 +34,12 @@ return {
 
     local builtin = require("telescope.builtin")
     local k = vim.keymap
+    local ignore_patterns = { "migrations/.*", "%.g%.dart$", "freezed%.dart$" }
     -- Update these lines to include file_ignore_patterns in the options
-    k.set("n", "<leader>ff", function() builtin.find_files({ file_ignore_patterns = { "migrations/.*" } }) end, {})
-    k.set("n", "<leader>fg", function() builtin.live_grep({ file_ignore_patterns = { "migrations/.*" } }) end, {})
-    k.set("n", "<leader>fc", function() builtin.grep_string({ file_ignore_patterns = { "migrations/.*" } }) end, {})
+    k.set("n", "<leader>ff",
+      function() builtin.find_files({ file_ignore_patterns = ignore_patterns }) end, {})
+    k.set("n", "<leader>fg", function() builtin.live_grep({ file_ignore_patterns = ignore_patterns }) end, {})
+    k.set("n", "<leader>fc", function() builtin.grep_string({ file_ignore_patterns = ignore_patterns }) end, {})
     k.set("n", "<leader>fb", builtin.buffers, {})
     k.set("n", "<leader>fh", builtin.help_tags, {})
     k.set("n", "<leader>fo", builtin.oldfiles, {})
@@ -45,15 +47,15 @@ return {
     k.set("n", "<leader>fcc", builtin.commands, {})
     k.set("n", "<leader>fm", builtin.marks, {})
     k.set("n", "<leader>ft",
-      function() builtin.current_buffer_fuzzy_find({ file_ignore_patterns = { "migrations/.*" } }) end, {})
-    k.set("n", "<leader>fs", function() builtin.lsp_document_symbols({ file_ignore_patterns = { "migrations/.*" } }) end,
+      function() builtin.current_buffer_fuzzy_find({ file_ignore_patterns = ignore_patterns }) end, {})
+    k.set("n", "<leader>fs", function() builtin.lsp_document_symbols({ file_ignore_patterns = ignore_patterns }) end,
       {})
-    k.set("n", "<leader>fw", function() builtin.lsp_workspace_symbols({ file_ignore_patterns = { "migrations/.*" } }) end,
+    k.set("n", "<leader>fw", function() builtin.lsp_workspace_symbols({ file_ignore_patterns = ignore_patterns }) end,
       {})
-    k.set("n", "<leader>fd", function() builtin.lsp_definitions({ file_ignore_patterns = { "migrations/.*" } }) end, {})
-    k.set("n", "<leader>fi", function() builtin.lsp_implementations({ file_ignore_patterns = { "migrations/.*" } }) end,
+    k.set("n", "<leader>fd", function() builtin.lsp_definitions({ file_ignore_patterns = ignore_patterns }) end, {})
+    k.set("n", "<leader>fi", function() builtin.lsp_implementations({ file_ignore_patterns = ignore_patterns }) end,
       {})
-    k.set("n", "<leader>fr", function() builtin.lsp_references({ file_ignore_patterns = { "migrations/.*" } }) end, {})
+    k.set("n", "<leader>fr", function() builtin.lsp_references({ file_ignore_patterns = ignore_patterns }) end, {})
     -- Registers
     k.set("n", "<leader>fz", builtin.registers, {})
   end
