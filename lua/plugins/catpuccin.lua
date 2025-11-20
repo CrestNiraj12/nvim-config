8a0e6c8cdbaa690e57330376afd1a48346c4cf75
Niraj Shrestha <crestniraj@gmail.com>
2025-11-21 00:52:22 +0545

    chore: updated ui

--- lua/plugins/catpuccin.lua
+++ lua/plugins/catpuccin.lua
@@ -5,18 +5,26 @@ return {
   config = function()
     require("catppuccin").setup({
       flavour = "mocha",
-      transparent_background = false,
       color_overrides = {
         mocha = {
-          base = "#12121b",
+          base = "#16161e",
           mantle = "#1a1a23",
           crust = "#101017",
         },
       },
+      custom_highlights = function(_)
+        return {
+          NeoTreeNormal       = { bg = "#13131c" },
+          NeoTreeNormalNC     = { bg = "#13131c" },
+          NeoTreeWinSeparator = { fg = "#13131c", bg = "#13131c" },
+        }
+      end,
       integrations = {
+        dap_ui = true,
+        dap = true,
         cmp = true,
         gitsigns = true,
-        nvimtree = true,
+        neotree = true,
         treesitter = true,
         telescope = true,
       }
