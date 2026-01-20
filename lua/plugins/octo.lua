8d76d7b814b738e0237dc2c61dfe637ad8a296ef
Niraj Shrestha <crestniraj@gmail.com>
2026-01-20 13:24:31 +0545

    ♻️ refactor(plugins): Integrate snacks.nvim plugin
    Add comprehensive snacks.nvim configuration with pickers, explorers,
    zen mode, notifications, and various utilities. Delete zen-mode.lua
    as snacks provides equivalent zen mode functionality. Update octo.lua
    to use snacks as picker and expand GitHub operation keymaps. Remove
    redundant keymaps from telescope.lua and lsp/keymaps.lua that are
    now provided by snacks.
    
    This consolidates plugin ecosystem and eliminates redundancy.

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -1,15 +1,38 @@
 return {
-	'pwntester/octo.nvim',
-	dependencies = {
-		'nvim-lua/plenary.nvim',
-		'nvim-telescope/telescope.nvim',
-		'nvim-tree/nvim-web-devicons',
-	},
-	config = function()
-		require("octo").setup({ enable_builtin = true })
-		vim.cmd([[hi OctoEditable guibg=none]])
-	end,
-	keys = {
-		{ "<leader>O",  "<cmd>Octo<cr>",         desc = "Octo" },
-	}
+  'pwntester/octo.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+    'nvim-tree/nvim-web-devicons',
+  },
+  cmd = "Octo",
+  opts = {
+    picker = "snacks",
+    enable_builtin = true,
+  },
+  keys = {
+    { "<leader>O", "<cmd>Octo<cr>", desc = "Octo" },
+    {
+      "<leader>op",
+      "<CMD>Octo pr list<CR>",
+      desc = "List GitHub PullRequests",
+    },
+    {
+      "<leader>od",
+      "<CMD>Octo discussion list<CR>",
+      desc = "List GitHub Discussions",
+    },
+    {
+      "<leader>on",
+      "<CMD>Octo notification list<CR>",
+      desc = "List GitHub Notifications",
+    },
+    {
+      "<leader>os",
+      function()
+        require("octo.utils").create_base_search_command { include_current_repo = true }
+      end,
+      desc = "Search GitHub",
+    },
+  }
 }
