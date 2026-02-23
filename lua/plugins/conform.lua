b54f4a07ce14fef3418936da9d888f4121917d3a
Niraj Shrestha <crestniraj@gmail.com>
2026-02-23 23:24:08 +0545

    ✨ feat(conform): Add React formatters
    Enable formatting for javascriptreact and typescriptreact file types
    utilizing prettierd and prettier with a stop_after_first configuration.

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -7,6 +7,8 @@ return {
 			rust = { "rustfmt", lsp_format = "fallback" },
 			javascript = { "prettierd", "prettier", stop_after_first = true },
 			typescript = { "prettierd", "prettier", stop_after_first = true },
+			javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+			typescriptreact = { "prettierd", "prettier", stop_after_first = true },
 		},
 		format_on_save = {
 			timeout_ms = 500,
