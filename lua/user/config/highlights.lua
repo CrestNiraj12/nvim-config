2a5cf34712eeddedf3690369f5dfa5cf372fdd74
Niraj Shrestha <crestniraj@gmail.com>
2026-03-06 18:41:45 +0545

    💄 style: Update Snacks highlight colors\n\nAdjust foreground color of SnacksIndentScope for better\ncompatibility with themes, and remove inline comments\nfrom highlight definitions.

--- lua/user/config/highlights.lua
+++ lua/user/config/highlights.lua
@@ -1,10 +1,10 @@
 vim.api.nvim_set_hl(0, "SnacksIndent", {
-	fg = "#3a3a3a", -- pick something close to your background
+	fg = "#3a3a3a",
 	bg = "NONE",
 })
 
 vim.api.nvim_set_hl(0, "SnacksIndentScope", {
-	fg = "#5a6ea8", -- soft blue tint
+	fg = "#8c5665",
 	bg = "NONE",
 	bold = false,
 })
